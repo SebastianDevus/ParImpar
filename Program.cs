9d3ff403f5ebe5e8c6ee27a8c04d920b14b577be
SebastianDevus <sebastianjogosextra@gmail.com>
2022-04-09 12:07:34 -0300

    Exxercício concluído

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int numero;
+
+// Espacamento
+Console.WriteLine();
+
+// Solicitando e armazenando número
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+// Testando se o número é par
+if (numero % 2 == 0)
+{ 
+    // Mensagem caso seja par
+    Console.WriteLine($"O número {numero} é par!");
+}
+else
+{
+    // Mensagem caso seja ímpar
+    Console.WriteLine($"O número {numero} é ímpar!");
+}
+
+// Espacamento
+Console.WriteLine();
